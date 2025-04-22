1e1676660689cad68695a47ddc35d5984e72a8bd
Ahmad hilmi <ahmadhilmi420>
2025-04-23 00:14:57 +0700

    update

---
+++ src/app/api/payment/route.ts
@@ -0,0 +1,17 @@
+// import { NextResponse } from "next/server";
+
+// export async function GET() {
+//   try {
+//     const payment = await prisma.payment.findMany();
+//     return NextResponse.json(payments);
+//   } catch (error) {
+//     console.error("Error fetching markets:", error);
+//     return NextResponse.json(
+//       { message: "Error fetching markets" },
+//       { status: 500 }
+//     );
+//   }
+// }
+
+// get 
+// post
