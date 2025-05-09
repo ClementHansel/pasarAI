8a8f2f7b7c70663b819fbff4582062e47c15ff71
clementhansel8891 <greedybugz@gmail.com>
2025-05-09 14:51:19 +0700

    modify auth guard, middleware, auth.ts to unify the session system

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -83,7 +83,7 @@ export const authOptions: NextAuthOptions = {
   ],
 
   session: {
-    strategy: "database",
+    strategy: "jwt",
     maxAge: 30 * 24 * 60 * 60, // 30 days
   },
   callbacks: {
